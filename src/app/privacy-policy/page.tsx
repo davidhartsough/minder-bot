0b72bf94068945b4376a6011db71708d29b20f04
davidhartsough <hartsoughdavid@gmail.com>
2024-09-18 18:38:23 +1000

    add privacy policy page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,270 @@
+export default function PrivacyPolicy() {
+  return (
+    <main>
+      <section>
+        <article id="privacy-policy" className="m-auto mb-8 max-w-3xl p-4">
+          <h1>The &quot;minder bot&quot; Privacy Policy</h1>
+          <p>
+            This privacy policy for When Tho LLC (&quot;we&quot;,
+            &quot;us&quot;, or &quot;our&quot;) explains how and why we collect,
+            store, and use your information when you use our website and
+            application services (&quot;website&quot;, &quot;application&quot;,
+            &quot;services&quot;).
+          </p>
+          <p>Contents:</p>
+          <ol>
+            <li>
+              <a href="#what-we-collect">What we collect</a>
+            </li>
+            <li>
+              <a href="#why-we-collect-it">Why we collect it</a>
+            </li>
+            <li>
+              <a href="#how-we-collect-it">How we collect it</a>
+            </li>
+            <li>
+              <a href="#how-we-store-it">How we store it</a>
+            </li>
+            <li>
+              <a href="#how-we-secured-it">How we secured it</a>
+            </li>
+            <li>
+              <a href="#how-long-we-retain-it">How long we retain it</a>
+            </li>
+            <li>
+              <a href="#who-has-access-to-it">Who has access to it</a>
+            </li>
+            <li>
+              <a href="#how-you-can-manage-it">How you can manage it</a>
+            </li>
+            <li>
+              <a href="#what-your-rights-are">What your rights are</a>
+            </li>
+            <li>
+              <a href="#what-cookies-are">What cookies are</a>
+            </li>
+            <li>
+              <a href="#how-we-use-cookies">How we use cookies</a>
+            </li>
+            <li>
+              <a href="#how-to-manage-your-cookies">
+                How to manage your cookies
+              </a>
+            </li>
+            <li>
+              <a href="#how-to-handle-other-websites">
+                How to handle other websites
+              </a>
+            </li>
+            <li>
+              <a href="#how-we-update-this-policy">How we update this policy</a>
+            </li>
+            <li>
+              <a href="#how-to-contact-us">How to contact us</a>
+            </li>
+          </ol>
+          <h2 id="what-we-collect">What we collect</h2>
+          <p>
+            We do not collect any personally identifiable information such as
+            your name, email, or contact details. The only data we store
+            includes:
+          </p>
+          <ul>
+            <li>Lists you create, including the titles and items.</li>
+            <li>
+              Unique IDs associated with your lists and your account for
+              technical purposes.
+            </li>
+          </ul>
+          <p>
+            This information is not linked to any personally identifiable data.
+          </p>
+
+          <h2 id="why-we-collect-it">Why we collect it</h2>
+          <p>We collect your data so that we can:</p>
+          <ul>
+            <li>Manage your account</li>
+            <li>Provide you with our services</li>
+            <li>Improve our services</li>
+          </ul>
+          <h2 id="how-we-collect-it">How we collect it</h2>
+          <p>
+            We collect this data either directly from you or our third party
+            auth platform: Facebook Login (from Meta).
+          </p>
+          <p>You directly provide us with the data we collect.</p>
+          <p>We store your data when you:</p>
+          <ul>
+            <li>Create an account via Facebook Login</li>
+            <li>Fill out forms within the application</li>
+            <li>Utilize features in the application</li>
+          </ul>
+          <h2 id="how-we-store-it">How we store it</h2>
+          <p>We securely store your data in databases managed by Vercel.</p>
+          <h2 id="how-we-secured-it">How we secured it</h2>
+          <p>
+            We protect your data in our databases by enforcing strict safeguards
+            that ensure your data is secure and to prevent unauthorized access.
+          </p>
+          <p>
+            Additionally, we do not store, save, or associate any personally
+            identifiable information with your data.
+          </p>
+          <h2 id="how-long-we-retain-it">How long we retain it</h2>
+          <p>
+            We will keep your account data for only as long as is necessary to
+            provide you with the services you request from the website and
+            application.
+          </p>
+          <h2 id="who-has-access-to-it">Who has access to it</h2>
+          <h3>Database access</h3>
+          <p>
+            Your data is stored securely in a database that is only accessible
+            to the webmasters as anonymized, serialized, and otherwise encrypted
+            data. Anyone with this access is subject to strict contractual and
+            legal confidentiality obligations.
+          </p>
+          <h3>In-app access</h3>
+          <p>
+            From within the application, your data is only accessible to you.
+          </p>
+          <h3>Company access</h3>
+          <p>We do NOT share your data with any other companies.</p>
+          <h2 id="how-you-can-manage-it">How you can manage it</h2>
+          <p>
+            You may manage your data either directly from the website
+            application or by emailing us your request.
+          </p>
+          <p>
+            Within the application you may update or delete any account data you
+            wish at any time, including the option to completely delete your
+            entire account and all of its data.
+          </p>
+          <p>
+            You may also email us to correct, delete, or request access to any
+            personal information that you have provided to us.
+          </p>
+          <p>
+            Upon your request, we can delete your data at any time for you by
+            completely removing it from our databases.
+          </p>
+          <h2 id="what-your-rights-are">What your rights are</h2>
+          <p>
+            Every user is entitled to the following data protection rights to:
+          </p>
+          <ul>
+            <li>
+              <strong>Access</strong>: You have the right to request copies of
+              your personal data.
+            </li>
+            <li>
+              <strong>Rectification</strong>: You have the right to request that
+              we correct any information you believe is inaccurate. You also
+              have the right to request for us to help you complete the
+              information you believe is incomplete.
+            </li>
+            <li>
+              <strong>Erasure</strong>: You have the right to request that we
+              erase your personal data, under certain conditions.
+            </li>
+            <li>
+              <strong>Restrict processing</strong>: You have the right to
+              request that we restrict the processing of your personal data,
+              under certain conditions.
+            </li>
+            <li>
+              <strong>Object to processing</strong>: You have the right to
+              object to our processing of your personal data, under certain
+              conditions.
+            </li>
+            <li>
+              <strong>Data portability</strong>: You have the right to request
+              that we transfer the data that we have collected to another
+              organization, or directly to you, under certain conditions.
+            </li>
+          </ul>
+          <p>
+            If you make a request, we have one month to respond to you. If you
+            would like to exercise any of these rights, please contact us at our
+            email:{" "}
+            <a
+              href="mailto:hartsoughdavid@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hartsoughdavid@gmail.com
+            </a>
+          </p>
+          <h2 id="what-cookies-are">What cookies are</h2>
+          <p>
+            Cookies are text files stored on your device&apos;s internal memory
+            that are used to collect standard Internet log information and
+            visitor information. When you visit our website, we may collect
+            information from you automatically through cookies.
+          </p>
+          <p>
+            For further information, visit{" "}
+            <a
+              href="https://allaboutcookies.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              allaboutcookies.org
+            </a>
+          </p>
+          <h2 id="how-we-use-cookies">How we use cookies</h2>
+          <p>
+            We strictly use cookies for authorization and authentication, which
+            allows our services to keep you signed while using our website.
+            Thus, these cookies are necessary for the functionality of our
+            services, as they are used to recognize you during and between
+            sessions.
+          </p>
+          <h2 id="how-to-manage-your-cookies">How to manage your cookies</h2>
+          <p>
+            You have the option to set your browser to either accept or refuse
+            these cookies and know when a cookie is being sent to your device.
+            However, if you choose to refuse our cookies, you will not be able
+            to fully use our services, because our website relies on the cookies
+            to function properly.
+          </p>
+          <h2 id="how-to-handle-other-websites">
+            How to handle other websites
+          </h2>
+          <p>
+            Our website may contain links to other websites. Our privacy policy
+            applies only to our website, so if you click on a link and go to
+            another website, you should read their privacy policy. These
+            external sites are not operated by us. We have no control over and
+            assume no responsibility for the content, privacy policies, or
+            practices of any third-party sites or services.
+          </p>
+          <h2 id="how-we-update-this-policy">How we update this policy</h2>
+          <p>
+            We keep our privacy policy under regular review and place any
+            updates on this web page. We may update our Privacy Policy from time
+            to time. Please revisit this page periodically to review any changes
+            we have posted here.
+          </p>
+          <p>This privacy policy was last updated on 18 September 2024.</p>
+          <h2 id="how-to-contact-us">How to contact us</h2>
+          <p>
+            If you have any questions about our privacy policy or the data we
+            hold on you, or if you would like to exercise one of your data
+            protection rights, please do not hesitate to contact us.
+          </p>
+          <p>
+            Email us at:{" "}
+            <a
+              href="mailto:hartsoughdavid@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hartsoughdavid@gmail.com
+            </a>
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+}
